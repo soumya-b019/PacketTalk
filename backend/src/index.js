17be34836d20077d97f2c53728c287a6f7a6b8b7
Soumya Bhandarkar <soumyabhandarkar2000@gmail.com>
2025-08-22 16:54:22 +0530

    Deployment setup done

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT ?? 8080;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +30,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,9 @@ import { create } from "zustand";
 import { axiosInstance } from "@/lib/axios.js";
 import toast from "react-hot-toast";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:8080" : "/";
+
 export const useAuthStore = create((set) => ({
   authUser: null,
   onlineUsers: [],
